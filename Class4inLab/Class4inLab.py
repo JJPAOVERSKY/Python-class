a718faaa47214ba9c800d6f2bd78f6f5d610b689
JJPAOVERSKY <57521892+JJPAOVERSKY@users.noreply.github.com>
2019-12-09 11:22:38 +0800

    Add files via upload

---
+++ Class4inLab/Class4inLab.py
@@ -0,0 +1,60 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+
+# t = np.arange(0, 2, 0.01)
+# print(t)
+# s = np.sin(2*np.pi*t)
+# plt.title("函数:y=sin(x)", fontsize=30)
+# plt.xlabel("x", fontsize=20)
+# plt.ylabel("sin(x)", fontsize=20)
+# plt.plot(s)
+# plt.grid()
+# plt.show()
+
+# ----------------------------------------
+# s = [1, 2, 3, 6, 9]
+# t = [2, 4, 6, 8, 10]
+# plt.plot(t, s)
+# plt.title("damn", fontsize=30)
+# plt.xlabel("t", fontsize=20)
+# plt.ylabel("s", fontsize=20)
+# plt.grid()
+# plt.show()
+# -----------------------------------------
+# plt.scatter(2, 4, s=22)
+# plt.show()
+# ------------------------------------------
+
+# x=[1, 2, 3, 4, 5]
+# y =[1, 4, 9, 16, 25]
+# plt.scatter(x, y, s=100)
+# plt.show()
+# ---------------------------------------------
+
+# x = list(range(1, 100))
+# y = [i**2 for i in x]
+# plt.scatter(x, y, s=10, c=y, cmap=plt.cm.Blues)
+# plt.show()
+
+# -----------------------------------------------
+
+# x = np.arange(0, 10, 0.1)
+# y = random
+# plt.scatter(x, y, s=10, c=y, cmap=plt.cm.Blues)
+# plt.show()
+# -----------------------------------
+import pandas as pd
+df = pd.read_excel('.\data_person.xlsx', 'Sheet1')
+x=df['Age']
+#plt.hist(x, bins=5)
+#plt.show()
+
+data = np.random.random(4)
+explode=[0.5, 0, 0, 0]
+label = 'frogs', 'apple', 'banana', 'shit'
+color = ['yellow', 'black', 'blue', 'gold']
+plt.pie(data, labels=label, colors=color, shadow=True, explode=explode)
+plt.show()
